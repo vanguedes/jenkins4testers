d64b52feada70a6c4d889e0b26fde67890e4dcb7
vanguedes <mvanguedes@gmail.com>
2019-08-28 15:00:02 -0300

    fixing drive chrome to run headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,12 +6,24 @@ require_relative "helpers"
 
 World(Helpers)
 
+# CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
+
 case ENV["BROWSER"]
 when "firefox"
   @driver = :selenium
 when "chrome"
   @driver = :selenium_chrome
 when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+
   @driver = :selenium_chrome_headless
 else
   puts "Invalid browser"
@@ -19,6 +31,6 @@ end
 
 Capybara.configure do |config|
   config.default_driver = @driver
-  config.app_host = "http://localhost:8080"
+  config.app_host = "http://localhost:8000"
   config.default_max_wait_time = 5
 end
